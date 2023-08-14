d80b94da8b77b54661c39d3489acfd7e2d408894
zundokoberoncho <zunbero_zundokoberoncho@yahoo.co.jp>
2023-08-14 16:38:30 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -31,12 +31,22 @@ class CalendarsController < ApplicationController
 
     7.times do |x|
       today_plans = []
+
+      # 曜日番号と曜日の文字列をここで取得
+      wday_num = (@todays_date + x).wday
+      wday_str = wdays[wday_num]
+
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      days = {
+        :month => (@todays_date + x).month,
+        :date => (@todays_date + x).day,
+        :plans => today_plans,
+        :wday => wday_str  # 曜日の情報を追加
+      }
       @week_days.push(days)
     end
+end
 
-  end
 end
